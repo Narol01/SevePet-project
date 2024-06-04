a79d74e70930f062342e9d3a4960880d5cc81385
narol01 <kmingogod@gmail.com>
2024-06-04 11:10:43 +0200

    removed unused service in SecurityConfig class

--- src/main/java/ait/cohort34/security/config/SecurityConfig.java
+++ src/main/java/ait/cohort34/security/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package ait.cohort34.security.config;
 
 import ait.cohort34.security.filter.TokenFilter;
-import ait.cohort34.security.service.AuthService;
 import ait.cohort34.security.service.CustomWebSecurity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +12,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -27,15 +28,17 @@ public class SecurityConfig {
             "/v3/api-docs.yaml",
             "/swagger-ui/**",
             "/swagger-ui.html"
-    };
-    final AuthService authService;
-    final CustomWebSecurity webSecurity;
+    };final CustomWebSecurity webSecurity;
     final private TokenFilter tokenFilter;
     @Autowired
-    public SecurityConfig(AuthService authService, CustomWebSecurity webSecurity, TokenFilter tokenFilter) {
-        this.authService = authService;
+    public SecurityConfig(CustomWebSecurity webSecurity, TokenFilter tokenFilter) {
         this.webSecurity = webSecurity;
         this.tokenFilter = tokenFilter;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
--- src/main/java/ait/cohort34/security/service/AuthService.java
+++ src/main/java/ait/cohort34/security/service/AuthService.java
@@ -1,16 +1,14 @@
 package ait.cohort34.security.service;
 
 import ait.cohort34.accounting.dto.UserAuthDto;
-import ait.cohort34.accounting.dto.UserRegisterDto;
 import ait.cohort34.accounting.model.UserAccount;
 import ait.cohort34.accounting.service.UserService;
 import ait.cohort34.security.AuthInfo;
 import ait.cohort34.security.dto.TokenResponseDto;
 import io.jsonwebtoken.Claims;
 import lombok.NonNull;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.naming.AuthenticationException;
@@ -21,18 +19,18 @@ public class AuthService {
 final private UserService userService;
 final private TokenService tokenService;
 final private Map<String,String> refreshStorage;
-final private BCryptPasswordEncoder encoder;
+final private PasswordEncoder passwordEncoder;
 
-public AuthService(UserService userService, TokenService tokenService,BCryptPasswordEncoder encoder) {
+public AuthService(UserService userService, TokenService tokenService, PasswordEncoder passwordEncoder) {
     this.userService = userService;
     this.tokenService = tokenService;
-    this.encoder = encoder;
+    this.passwordEncoder = passwordEncoder;
     this.refreshStorage=new HashMap<>();
 }
 public TokenResponseDto login(@NonNull UserAuthDto inboundUser) throws AuthenticationException {
     String username = inboundUser.getLogin();
     UserAccount foundUser=(UserAccount) userService.loadUserByUsername(username);
-    if(encoder.matches(inboundUser.getPassword(), foundUser.getPassword())) {
+    if(passwordEncoder.matches(inboundUser.getPassword(), foundUser.getPassword())) {
         String accessToken = tokenService.generateAccessToken(foundUser);
         String refreshToken = tokenService.generateRefreshToken(foundUser);
         refreshStorage.put(username, refreshToken);
