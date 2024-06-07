99538ec988f0cf7f8981ec4e3a64f6db8feacad2
narol01 <kmingogod@gmail.com>
2024-06-07 22:11:53 +0200

    edit and register

--- src/main/java/ait/cohort34/accounting/dto/NewPasswordDto.java
+++ src/main/java/ait/cohort34/accounting/dto/NewPasswordDto.java
@@ -1,5 +1,8 @@
 package ait.cohort34.accounting.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,6 +11,9 @@ import lombok.Setter;
 public class NewPasswordDto {
 
     String oldPassword;
+    @NotEmpty(message = "Password must not be empty")
+    @Size(min = 4, max = 8, message = "Password must be at least 4-8 characters long")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d).+$", message = "Password must contain at least one uppercase letter, one lowercase letter, and one digit")
     String newPassword;
 
 }
